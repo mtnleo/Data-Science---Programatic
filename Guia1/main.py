722b14c0af7f784625441c8609eb6a1249ef2bfc
mtnleo <mtnleonardi@gmail.com>
2023-05-20 15:25:46 -0300

    Guia 1 finalizada

--- Guia1/main.py
+++ Guia1/main.py
@@ -1,4 +1,4 @@
-import math
+import math, functools as ft
 
 def Ejercicio1(): 
     num = int(input("Escribi un numero: "))
@@ -60,8 +60,73 @@ def Ejercicio7():
 
     print(f"El mayor numero fue {mayor}\nEl menor numero fue {menor}")
 
+def Ejercicio8(cant_nums):
+    lista_numeritos = [int(input("Escriba un numero: ")) for i in range(0, cant_nums)]
+    print(f"Resultado de la multiplicacion de dichos numeros: {ft.reduce(lambda suma, num: suma * num, lista_numeritos)}")
 
+def Ejercicio9():
+    USUARIO = "TINI"
+    CONTRASENIA = "GUOLIS123"
 
+    usu_guess = input("Nombre: ")
+    pass_guess = input("Password: ")
+
+    if (USUARIO == usu_guess and CONTRASENIA == pass_guess):
+        print("Bienvenidx!")
+    else:
+        print("Intente de nuevo!")
+        if (USUARIO != usu_guess):
+            print("Verificar usuario")
+        if (CONTRASENIA != pass_guess):
+            print("Verificar contrasenia")
+
+def Ejercicio10():
+    suma = 0
+    num = -1
+    while num != 0:
+        num = int(input("Ingrese un numero (0 para salir): "))
+        if (num != 0):
+            suma += num
+
+    print(f"La suma final es de {suma}")
+
+def Ejercicio11():
+    MESES = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    ganancia_mensual = dict()
+    facturacion_total = 0
+    coste_total = 0
+    gasto_mes = 0
+    ingreso_mes = 0
+
+    for mes in MESES:
+        print(f"{mes}.")
+        ingreso_mes = int(input("Ingreso: "))
+        gasto_mes = int(input("Gasto: "))
+
+        facturacion_total += ingreso_mes
+        coste_total += gasto_mes
+        ganancia_mensual.update({mes: ingreso_mes - gasto_mes}) 
+
+    print("Facturacion anual: ", facturacion_total)
+    print("Ganancia promedio = ", facturacion_total / 12)
+    print("Ganancia mensual: ", ganancia_mensual)
+
+def Ejercicio12():
+    n1 = int(input("Ingrese un valor: "))
+    n2 = int(input("Ingrese otro valor: "))
+    op = input("Ingrese operacion a realizar (+, -, /, *): ")
+
+    if (op == '+'):
+        print(n1 + n2)
+    elif (op == '-'):
+        print(n1 - n2)
+    elif (op == '*'):
+        print(n1 * n2)
+    elif (op == '/'):
+        print(n1 / n2)
+    else:
+        print("Elija un operador valido!")
 
 if __name__ == "__main__":
-    Ejercicio7()
+    # ir hardcoding q ejercicio se hace :)
+    Ejercicio1()
